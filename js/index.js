05614a4d3e19f10b568c4e78a437d327bc3a7d98
aidabyte <ablinstrubyte@gmail.com>
2022-08-02 17:52:22 -0500

    made alot of changes to index

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    $(".clickMyResume").mouseover(function(){
+      $(this).animate({  center: '250px',
+      opacity: '5.5',
+      height: '100px',
+      width: '250px',
+      fontSize: "50px"},"fast");
+      
+    });
+    });
+;
+$(".clickMyResume").mouseout(function(){
+  $(this).animate({  center: '150px',
+  opacity: '5.5',
+  height: '50px',
+  width: '150px',
+  fontSize: "20px"},"fast");
+  
+});
